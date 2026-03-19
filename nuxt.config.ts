b842c774ab8459aa1e7d2f9920efa54393439357
Nahaq789 <naha022677@icloud.com>
2026-03-19 16:35:04 +0900

    fix: format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   ssr: false,
-  compatibilityDate: '2026-03-19',
-  modules: ['@pinia/nuxt'],
-  css: ['~/assets/css/main.css'],
+  compatibilityDate: "2026-03-19",
+  modules: ["@pinia/nuxt"],
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
-})
+});
